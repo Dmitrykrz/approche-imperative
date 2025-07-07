fef0a11d30a7a8449c532f247a416c9d2643fd06
Dmitry <Dmitrykorzhenevskiy@gmail.com>
2025-07-07 12:02:07 +0200

    Forgot to add package

--- src/fr/declaration/variable/DeclarationApp.java
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -1,26 +1,29 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+    public static void main(String[] args) {
 
 
+        byte a = 4;
+        System.out.println("a = " + a);
 
-public static void main (String[] args)   {
-    byte a=4;
-    System.out.println("a = " + a);
+        int b = 4;
+        System.out.println("b = " + b);
 
-    int b=4;
-    System.out.println("b = " + b);
+        float f = 4.4F;
+        System.out.println("f = " + f);
 
-    float f=4.4F;
-    System.out.println("f = " + f);
+        double l = 4.6;
+        System.out.println("l = " + l);
 
-    double l=4.6;
-    System.out.println("l = " + l);
+        boolean w = false;
+        System.out.println("w = " + w);
 
-    String q="Hello";
-    System.out.println("q = " + q);
-
-    boolean w=false;
-    System.out.println("w = " + w);
+        String q = "Hello";
+        System.out.println("q = " + q);
 
 
-
+    }
 
 }
