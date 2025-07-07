960ccd2ed132c11fc44b263943da756deb30cb24
Dmitry <Dmitrykorzhenevskiy@gmail.com>
2025-07-07 11:43:46 +0200

    First commit

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,26 @@
+
+
+
+public static void main (String[] args)   {
+    byte a=4;
+    System.out.println("a = " + a);
+
+    int b=4;
+    System.out.println("b = " + b);
+
+    float f=4.4F;
+    System.out.println("f = " + f);
+
+    double l=4.6;
+    System.out.println("l = " + l);
+
+    String q="Hello";
+    System.out.println("q = " + q);
+
+    boolean w=false;
+    System.out.println("w = " + w);
+
+
+
+
+}
